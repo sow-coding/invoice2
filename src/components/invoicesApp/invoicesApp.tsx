4a38a2e8133a302cb17bf2522dba377965cca8ba
Saikou Sow <sow4work@gmail.com>
2024-02-06 11:29:44 +0100

    calc issue on mobile, solution 1

--- src/components/invoicesApp/invoicesApp.tsx
+++ src/components/invoicesApp/invoicesApp.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import NavBar from '../navBar/navBar'
 import { useInvoicesContext } from '@/contexts/invoices.context'
 import InvoiceList from '../invoiceList/invoiceList'
@@ -8,8 +9,24 @@ import { useInvoiceFormContext } from '@/contexts/invoiceForm.context'
 function InvoicesApp() {
   const {invoices} = useInvoicesContext()
   const {invoiceFormDisplayed} = useInvoiceFormContext()
+  const [displayNone, setDisplayNone] = useState<boolean>(false)
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 1021) {
+        setDisplayNone(true);
+      } else {
+        setDisplayNone(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
-    <div className='invoicesApp'>
+    <div className={`invoicesApp ${(invoiceFormDisplayed && displayNone) && "displayNone"}`}>
         <NavBar />
         {invoices.length === 0 ? <Nothing /> : 
         <InvoiceList />
